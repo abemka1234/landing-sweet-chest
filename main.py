6812d52414aefdb7cd8192fd2d399eb1fbd8cbfc
abemka1234 <alesparfenov@gmail.com>
2026-05-16 20:37:10 +0300

    Readme

--- main.py
+++ main.py
@@ -83,4 +83,4 @@ def Send_email():
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
